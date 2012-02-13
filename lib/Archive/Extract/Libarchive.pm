c3ef4a71b478d47329af58bb2b490eba2f2a89f2
Leon Brocard <acme@astray.com>
2012-02-13 21:09:34 +0000

    And release 0.38

--- lib/Archive/Extract/Libarchive.pm
+++ lib/Archive/Extract/Libarchive.pm
@@ -3,7 +3,7 @@ use strict;
 use warnings;
 use Cwd qw(cwd);
 use Object::Tiny qw{ archive extract_path files error };
-our $VERSION = '0.37';
+our $VERSION = '0.38';
 
 require XSLoader;
 XSLoader::load( 'Archive::Extract::Libarchive', $VERSION );
